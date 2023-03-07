6bfe665ab6e62a9ca89c9ffa43e47660f7e95523
aabughosh <aabugosh@redhat.com>
2023-03-07 16:32:55 +0200

    test the adding of the test deployment

--- controllers/foo_controller.go
+++ controllers/foo_controller.go
@@ -74,13 +74,13 @@ func (r *FooReconciler) Reconcile(ctx context.Context, req ctrl.Request) (ctrl.R
 			Replicas: &size,
 			Selector: &metav1.LabelSelector{
 				MatchLabels: map[string]string{
-					"app": "testdp",
+					"app": "jack",
 				},
 			},
 			Template: corev1.PodTemplateSpec{
 				ObjectMeta: metav1.ObjectMeta{
 					Labels: map[string]string{
-						"app":                                 "testdp",
+						"app":                                 "jack",
 						"test-network-function.com/generic":   "target",
 						"test-network-function.com/container": "target",
 					},
